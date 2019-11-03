8a917503079e8e9934b74206cb87dce7295a78db
Erno Seppänen <ernseppa@gmail.com>
2019-11-03 16:36:43 +0000

    kurssitiedot valmis.

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -58,7 +58,7 @@ const App = () => {
     <div>
       <Header course={course}/>
       {/*Muistoksi tyhmä toteutus.
-      -------------------------------------------------
+      --------------------------------------------------------
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
