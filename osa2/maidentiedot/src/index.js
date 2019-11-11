00744453c820391cbdf0f4cf4a70fa5d9a7df2a4
Erno Seppänen <ernseppa@gmail.com>
2019-11-11 18:49:27 +0200

    2.13 kesken.

--- osa2/maidentiedot/src/index.js
+++ osa2/maidentiedot/src/index.js
@@ -3,47 +3,58 @@ import ReactDOM from 'react-dom';
 import axios from 'axios'
 import Filter from './components/Filter'
 
+const Button = ({onClick}) => {
+    return (
+        <button onClick={onClick}>show</button>
+    )
+}
 
-const ShowCountries = ({countries, search}) => {
-    const filteredBySearch = countries.filter(country => country.name.toLocaleLowerCase().includes(search))
-    console.log("Debug", countries[0])
+const ShowCountry = ({countries}) => {
+    return (
+        <div>{countries.map(country =>
+            <div key={country.alpha3Code}>
+              <h1>{country.name}</h1> 
+              <p>Capital {country.capital}</p>
+              <p>Population {country.population}</p>
+              <h3>Languages</h3>
+              <ul>
+                  {countries.map(country => 
+                    <div key={country.name}>  
+                        {country.languages.map((language, index) => 
+                             <li key={index}>
+                                 {language.name}
+                             </li>)}
+                    </div>)}
+             </ul>
+            <img src={country.flag} style={{width:20 + '%'}} alt={'flag'}></img>
+            </div>)}
+       </div>
+    )
+}
 
-    if (filteredBySearch.length > 10) {
+const ShowCountries = ({countries, onClick}) => {
+    
+    if (countries.length > 10) {
         return (
             <div>Too many matches, specify another filter.</div>
         )
     }
 
-    if (filteredBySearch.length === 1) {
+    if (countries.length === 1) {
         return (
-            <div>{filteredBySearch.map(country =>
-                <div key={country.alpha3Code}>
-                  <h1>{country.name}</h1> 
-                  <p>Capital {country.capital}</p>
-                  <p>Population {country.population}</p>
-                  <h3>Languages</h3>
-                  <ul>
-                      {filteredBySearch.map(country => 
-                        <div key={country.name}>  
-                            {country.languages.map((language, index) => 
-                                 <li key={index}>
-                                     {language.name}
-                                 </li>)}
-                        </div>)}
-                 </ul>
-                <img src={country.flag} style={{width:20 + '%'}} alt={'flag'}></img>
-                </div>)}
-           </div>  
+            <ShowCountry countries={countries}/> 
         )
     }
     return (
-        <div>{filteredBySearch.map(country => <div key={country.alpha3Code}> {country.name} </div>)}</div>
+        <div>{countries.map((country, index) =>
+             <div key={index}> {country.name} <Button onClick={onClick}/> </div>)}</div>
     )
 }
 
 const App = () => {
     const [ countries, setCountries] = useState([])
     const [ newSearch, setNewSearch] = useState('')
+    const filteredBySearch = countries.filter(country => country.name.toLocaleLowerCase().includes(newSearch))
 
     useEffect(() => {
         axios.get('https://restcountries.eu/rest/v2/all').then(response => {setCountries(response.data)})
@@ -55,12 +66,15 @@ const App = () => {
       setNewSearch(event.target.value)
      }
 
+     const handleButtonClick = () => {
+         console.log('clicked')
+     }
 
 
     return (
         <div>
         <Filter onChange={handleSearchChange} />
-        <ShowCountries countries={countries} search={newSearch} />
+        <ShowCountries countries={filteredBySearch} onClick={handleButtonClick}/>
         </div>
 
     )
