d892648272333a9cd28d0228ec1004b6683e642a
Erno Seppänen <ernseppa@gmail.com>
2019-11-30 04:22:46 +0200

    4.2 step 2 tehty.

---
+++ osa4/backend/utils/middleware.js
@@ -0,0 +1,36 @@
+const morgan = require('morgan')
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+})
+
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+  morgan
+}
