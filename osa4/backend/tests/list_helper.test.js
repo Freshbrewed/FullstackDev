e519ec6b63244114619234ab6f1a689896e53a0f
Erno Seppänen <ernseppa@gmail.com>
2019-12-01 05:34:46 +0200

    4.6 step4 tehty.

---
+++ osa4/backend/tests/list_helper.test.js
@@ -0,0 +1,153 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const emptyBlog = []
+
+  const result = listHelper.dummy(emptyBlog)
+  expect(result).toBe(1)
+})
+
+
+describe('Total likes', () => {
+  test('of a empty list is zero', () => {
+    const emptyBlog = []
+    expect(listHelper.totalLikes(emptyBlog)).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    expect(listHelper.totalLikes(oneBlog)).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36)
+  })
+})
+
+
+describe('Favorite Blog', () => {
+  test('of empty list is null', () => {
+    const emptyBlog = []
+    expect(listHelper.favoriteBlog(emptyBlog)).toEqual(null)
+  })
+
+  test('of one blog is the blog itself', () => {
+    expect(listHelper.favoriteBlog(oneBlog)).toEqual(oneBlog[0])
+  })
+
+  test('of a bigger list is calculated right', () => {
+    expect(listHelper.favoriteBlog(blogs)).toEqual(blogs[2])
+  })
+})
+
+
+describe('Most blogs by author', () => {
+  test('of empty list is null', () => {
+    const emptyBlog = []
+    expect(listHelper.mostBlogs(emptyBlog)).toEqual(null)
+  })
+
+  test('of one blog is the blog itself', () => {
+    const blog = [
+      {
+        author: 'Edsger W. Dijkstra',
+        blogs: 1
+      }
+    ]
+    expect(listHelper.mostBlogs(oneBlog)).toEqual(blog)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const blog = [
+      {
+        author: 'Robert C. Martin',
+        blogs: 3
+      }
+    ]
+    expect(listHelper.mostBlogs(blogs)).toEqual(blog)
+  })
+})
+
+
+describe('Most likes on all blogs by author', () => {
+  test('of empty list is null', () => {
+    const emptyBlog = []
+    expect(listHelper.mostLikes(emptyBlog)).toEqual(null)
+  })
+
+  test('of one blog is the blog itself', () => {
+    const blog = [
+      {
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      }
+    ]
+    expect(listHelper.mostLikes(oneBlog)).toEqual(blog)
+  })
+})
+
+
+
+
+
+
+const blogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  }
+]
+
+const oneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
---
+++ osa4/backend/utils/list_helper.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-unused-vars */
+
+const dummy = (blogs) => {
+  return 1
+}
+
+
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+  const likes = blogs.map(element => element.likes)
+  const combinedLikes = likes.reduce((sum, element) => {
+    return sum + element
+  }, 0)
+  return combinedLikes
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  const blogWithMostLikes = blogs.reduce((previous, current) => {
+    return (previous.likes > current.likes) ? previous : current
+  }, 0)
+  return blogWithMostLikes
+}
+
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const authors = blogs.map(blog => blog.author)
+  const winningAuthor = authors.sort((a, b) => {
+    authors.filter(element => element === a).length
+    - authors.filter(element => element ===b).length
+  }).pop()
+  const authorWithBlogs = blogs.filter(blog => blog.author === winningAuthor)
+  console.log(winningAuthor)
+  console.log(authorWithBlogs)
+  let count = 0
+  authorWithBlogs.forEach(() => {
+    count += 1
+  })
+
+  return [
+    {
+      author: winningAuthor,
+      blogs: count
+    }
+  ]
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return 1
+}
+
+
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
