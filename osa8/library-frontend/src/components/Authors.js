6c5c260ee4b6d88a39060d473bc6d278c71d6f63
Erno Seppänen <ernseppa@gmail.com>
2020-09-23 18:58:19 +0300

    8.11 tehty

--- osa8/library-frontend/src/components/Authors.js
+++ osa8/library-frontend/src/components/Authors.js
@@ -1,11 +1,51 @@
   
-import React from 'react'
+import React, { useState } from 'react'
+import { gql, useMutation } from '@apollo/client'
+
+const EDIT_AUTHOR = gql`
+  mutation changeBornYear($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo)  {
+      name
+      born
+      id
+    }
+  }
+`
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors  {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
 
 const Authors = (props) => {
+  const [ author, setAuthor ] = useState('')
+  const [ born, setBorn ] = useState('')
+
+  const [ changeBornYear ] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [ 
+      { query: ALL_AUTHORS },
+    ]
+  })
+
   if (!props.show) {
     return null
   }
+
   const authors = [ ...props.authors]
+
+  const update = async (event) => {
+    event.preventDefault()
+
+    changeBornYear({ variables: { name: author, setBornTo: parseInt(born) } })
+    setAuthor('')
+    setBorn('')
+  }
+
 
   return (
     <div>
@@ -30,7 +70,20 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
-
+            <h3>Set birthyear</h3>
+            <form onSubmit={update}>
+              <div>
+                Name
+                <input value={author} onChange={({ target }) => setAuthor(target.value)}/>
+              </div>
+              <div>
+                Born
+                <input type="number" value={born} onChange={({ target }) => setBorn(target.value)}/>
+              </div>
+              <div>
+                <button type="submit">Update author</button>
+              </div>
+            </form>
     </div>
   )
 }
