21dc35925ac3ca9d394f1330f42e0e846cc0155b
Erno Seppänen <ernseppa@gmail.com>
2020-10-29 14:59:32 +0200

    9.9 tehty

--- osa9/expressTS/index.ts
+++ osa9/expressTS/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
+});
+
+app.get('/api/patients', (_req, _res) => {
+  console.log('this is not implemented yet');
+
+  
 });
 
 app.listen(PORT, () => {
