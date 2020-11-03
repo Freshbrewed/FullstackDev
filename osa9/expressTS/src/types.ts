952e0636fc0007948ed91b5aa0b5d792e1f443c1
Erno Seppänen <ernseppa@gmail.com>
2020-11-04 01:29:41 +0200

    9.19 tehty

--- osa9/expressTS/src/types.ts
+++ osa9/expressTS/src/types.ts
@@ -1,3 +1,4 @@
+
 export interface Diagnose {
     code: string;
     name: string;
@@ -8,10 +9,7 @@ export interface Diagnose {
     Male = 'male',
     Female = 'female'
   }
-  
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
+
   export interface Patient {
     id: string;
     name: string;
@@ -22,12 +20,56 @@ export interface Entry {
     entries: Entry[]
   }
 
+  interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnose['code']>;
+  }
+
+  export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+  }
+  
+  interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+  }
+
+  interface SickLeave {
+    startDate: string;
+    endDate: string;
+  }
+
+  interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: SickLeave;
+  }
+
+  interface Discharge  {
+    date: string;
+    criteria: string;
+  }
+
+  interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge;
+  }
+
+  export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
   export type NonSensitivePatient = Omit<Patient, 'ssn'>;
 
   export type NewPatient = Omit<Patient, 'id'>;
 
   export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >;
 
- /* export interface NewPatient extends Omit<Patient, 'id' | 'gender'> {
-    gender: string;
-  }*/
+ 
